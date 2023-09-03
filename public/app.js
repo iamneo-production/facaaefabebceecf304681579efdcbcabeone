b0674e60b8fe3c7934ea85382ef1434f13207620
Iamneo <techadmin@iamneo.ai>
2023-09-03 17:31:23 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -16,45 +16,45 @@ let conditions = [
 
 // Function to handle player moves
 const ticTacToe = (element, index) => {
-    // Check if the cell is already filled
-    if (cells[index] !== '') return;
+    // Your game logic here
 
-    // Update the game state
-    cells[index] = currentPlayer;
-    element.textContent = currentPlayer;
+    /*
+    **Part 1: Winning Conditions (Add your code here)**
 
-    // Check for winning conditions
-    for (const condition of conditions) {
-        const [a, b, c] = condition;
-        if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
-            // Display the winning message
-            result.textContent = `${currentPlayer} wins!`;
+    1. Implement the logic to check for winning conditions using the 'conditions' array.
+    2. Display a winning message in the 'result' element when a player wins.
+    3. Disable all buttons after a win.
+    */
 
-            // Disable all buttons after a win
-            btns.forEach(btn => btn.disabled = true);
-            return; // Exit the function to prevent further moves
-        }
-    }
+    // Your code to update the game state and check for a win
+    // ...
 
-    // Update the current player's turn
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    result.textContent = `Current Player: ${currentPlayer}`;
+    // Your code to display the current player's turn
+    // ...
+
+    // Your code to handle button and cell interactions
+    // ...
 };
+
+    /*
+    **Part 2: Reset Function (Add your code here)**
+
+    1. Implement a new function that resets the game to its initial state.
+    2. Ensure the 'cells', 'btns', and 'currentPlayer' variables are reset.
+    3. Update the 'result' element to indicate the current player's turn.
+    4. Re-enable all buttons for a new game.
+    */
 
 // Function to reset the game
 const resetGame = () => {
-    // Reset game state
-    cells = ['', '', '', '', '', '', '', '', ''];
-    currentPlayer = 'X';
+    // Your code to reset the game state
+    // ...
 
-    // Clear X and O symbols from cells on the game board
-    btns.forEach((btn, i) => {
-        btn.textContent = '';
-        btn.disabled = false;
-    });
+    // Your code to update the 'result' element
+    // ...
 
-    // Update the result element
-    result.textContent = 'Current Player: X';
+    // Your code to re-enable buttons
+    // ...
 };
 
 btns.forEach((btn, i) => {
